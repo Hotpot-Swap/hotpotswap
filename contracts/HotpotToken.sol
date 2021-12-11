93994b81c89825a5d6e9a7617c6fc5f6056f6fd4
Quang Vượng <hotpotswap.finance@gmail.com>
2021-12-11 23:04:18 +0700

    re deploy token

--- contracts/HotpotToken.sol
+++ contracts/HotpotToken.sol
@@ -4,14 +4,29 @@ pragma solidity 0.6.12;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
 // WARNING: There is a known vuln contained within this contract related to vote delegation, 
 // it's NOT recommmended to use this in production.  
 
 // HotpotToken with Governance.
-contract HotpotToken is ERC20("HotpotToken", "HOTPOT"), Ownable {
+contract HotpotToken is ERC20, Ownable, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    constructor() public ERC20("HotpotToken", "HOTPOT") {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    modifier onlyMinter() {
+        require(
+            hasRole(MINTER_ROLE, msg.sender),
+            "Caller is not the minter"
+        );
+        _;
+    }
+
     /// @notice Creates `_amount` token to `_to`. Must only be called by the owner (MasterChef).
-    function mint(address _to, uint256 _amount) public onlyOwner {
+    function mint(address _to, uint256 _amount) public onlyMinter {
         _mint(_to, _amount);
         _moveDelegates(address(0), _delegates[_to], _amount);
     }
